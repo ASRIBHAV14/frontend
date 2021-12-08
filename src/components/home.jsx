0839199dadcd5a945efa7c067b8868f50c65ac98
ASRIBHAV14 <93983214+ASRIBHAV14@users.noreply.github.com>
2021-12-08 14:37:01 +0530

    Add files via upload
    commited changes

---
+++ src/components/home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+
+
+class Home extends React.Component {
+    render() {
+        return <div className="container mt-5">
+
+            <h3>Welcome to Library Management System</h3>
+        </div>;
+    }
+}
+
+export default Home;
